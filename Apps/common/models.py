4d0bc5422e1efd4a9aa95f4cbbaa0689a575e554
Yusuf <sanjar.meb@gmail.com>
2025-04-24 13:39:00 +0500

    feat,fix

--- Apps/common/models.py
+++ Apps/common/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import re
+from django.core.exceptions import ValidationError
+from django.utils.crypto import get_random_string
 
 class BaseModel(models.Model):
     # Замена стандартного автоинкрементного ID на собственное поле
@@ -10,6 +13,18 @@ class BaseModel(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.id = self.generate_unique_id()
+        super().save(*args, **kwargs)
+
+    @classmethod
+    def generate_unique_id(cls):
+        while True:
+            new_id = get_random_string(length=6, allowed_chars='0123456789')
+            if not cls.objects.filter(id=new_id).exists():
+                return new_id
 
     class Meta:
         abstract = True
--- Apps/users/models.py
+++ Apps/users/models.py
@@ -2,24 +2,22 @@ from django.db import models
 from Apps.common.models import BaseModel
 import re
 from django.core.exceptions import ValidationError
-from django.utils.crypto import get_random_string
+from django.contrib.auth.models import AbstractUser
 
-class User(BaseModel):
 
-    email = models.EmailField(unique=True)
-
-    username = models.CharField(max_length=255)
+class User(AbstractUser, BaseModel):
 
     is_employer = models.BooleanField(default=False)
     is_verified = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
 
     phone_number = models.CharField(max_length=12,
                                     unique=True,
                                     blank=True,
                                     null=True
                                     )
+
+    date_joined = None # переопределение что-бы избежать повторения
+
     verification_code = models.CharField(max_length=5,
                                          blank=True,
                                          null=True
@@ -34,17 +32,25 @@ class User(BaseModel):
                 raise ValidationError("Неверный формат номера телефона. Номер должен быть в международном формате.")
         super().clean()
 
-    def save(self, *args, **kwargs):
-        if not self.id:
-            self.id = self.generate_unique_id()
-        super().save(*args, **kwargs)
-
-    @staticmethod
-    def generate_unique_id():
-        while True:
-            new_id = get_random_string(length=6, allowed_chars='0123456789')
-            if not User.objects.filter(id=new_id).exists():
-                return new_id
-
     def __str__(self):
-        return f"{self.username} ({self.get_role_display()})"
+        return f"{self.username}"
+
+
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(User,
+                             on_delete=models.CASCADE,
+                             verbose_name="пользователь"
+                             )
+    bio = models.TextField(max_length=2000,
+                           blank=True,
+                           null=True
+                           )
+
+    def user_directory_path(instance, filename):
+        # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+        return "user_{0}/{1}".format(instance.user.id, filename)
+
+    resume = models.FileField(upload_to=user_directory_path)
+
