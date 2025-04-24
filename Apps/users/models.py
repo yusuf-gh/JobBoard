ebce4dbe737986d426c72ee406882c392675a342
Yusuf <sanjar.meb@gmail.com>
2025-04-24 14:36:13 +0500

    feat

--- Apps/users/models.py
+++ Apps/users/models.py
@@ -49,8 +49,11 @@ class Profile(models.Model):
                            )
 
     def user_directory_path(instance, filename):
-        # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+
         return "user_{0}/{1}".format(instance.user.id, filename)
 
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
     resume = models.FileField(upload_to=user_directory_path)
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    photo = models.ImageField(upload_to=user_directory_path)
 
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-pwc_173ae2@a$t!82%do*lq6e$2mawmy3b(3@-0b_1&qdfe7qu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,6 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
